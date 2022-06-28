e5b78f0da578adc8b13534ae88ff650a9c2643b2
emmanuelmav <102841993+emmanuelmav@users.noreply.github.com>
2022-06-28 19:08:45 -0400

    Add files via upload

---
+++ AdviceGeneratorApp/app.js
@@ -0,0 +1,35 @@
+const quote = document.querySelector('.advice');
+const dice = document.querySelector('.img-container');
+const adviceId = document.querySelector('h3');
+
+
+// Create an async function to Gather data from API
+async function getQuote() {
+    // Handle Errors through the try & catch
+    try {
+        // Call the fetch API method
+        const response = await fetch('https://api.adviceslip.com/advice');
+        // Call the json() method
+        const data = await response.json();
+        // Store the quote into a varioble
+        const advice = data.slip.advice;
+        const id = data.slip.id;
+        // Add a new Quote to the paragraph
+        quote.textContent = `"${advice}"`;
+        adviceId.textContent = `Advice #${id}`;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// Create function that displays the quote
+function displayQuote() {
+    // Every time the button is clicked 
+    dice.addEventListener('click', () => {
+        // Call the Function
+        getQuote();
+    })
+}
+
+getQuote();
+displayQuote();
